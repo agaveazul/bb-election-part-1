c8f693007f39098df4f8d72a65cc959a8fd69d4d
Richard Strauss <rstrauss91@gmail.com>
2017-02-17 15:02:39 -0500

    Create communication to JSON api

--- election.js
+++ election.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
 
-  // Imagination!
+$.ajax({
+  url: 'https://bb-election-api.herokuapp.com/',
+  method: 'GET',
+  dataType: 'json'
+}).done(function(data) {
+  data["candidates"].forEach(function(candidate) {
+    var listEl = $('<li>').append(candidate.name + ", " + candidate.votes);
+    $('#sb').append(listEl);
+    });
+  });
 
-});
+})
